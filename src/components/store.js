cde072cbdc0986e4b1f1c6183502ada9d754733e
Farhad-Toumarzade <farhad.thw2@gmail.com>
2023-12-19 15:30:53 +0330

    Async actions in redux toolkit

--- src/components/store.js
+++ src/components/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cakeReducer from "../features/Cake/cakeSlice";
 import mikReducer from "../features/mik/mikSlice";
+import userReducer from "../features/user/userReducer";
 
 const store = configureStore({
   reducer: {
     cake: cakeReducer,
     milk: mikReducer,
+    users: userReducer,
   },
 });
 
---
+++ src/features/user/userSlice.js
@@ -0,0 +1,47 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const getAsyncUsers = createAsyncThunk(
+  "user/getAsyncUsers",
+  async (_, { rejectWithValue }) => {
+    try {
+      const response = await axios.get(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+      return response.data;
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  }
+);
+
+const initialState = {
+  loading: false,
+  data: [],
+  error: "",
+};
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  extraReducers: {
+    [getAsyncUsers.pending]: (state, action) => {
+      state.loading = true;
+      state.data = [];
+      state.error = "";
+    },
+    [getAsyncUsers.fulfilled]: (state, action) => {
+      state.data = action.payload;
+      state.loading = false;
+    },
+
+    [getAsyncUsers.rejected]: (state, action) => {
+      state.error = action.payload;
+      state.data = [];
+    },
+  },
+});
+
+export const {} = userSlice.actions;
+
+export default userSlice.reducer;
